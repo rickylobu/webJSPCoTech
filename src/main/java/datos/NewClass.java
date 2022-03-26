94b5d34fb622154480dc64fee026937a4e3e94b8
Ricardo Bueno <rickylobu@gmail.com>
2022-03-25 22:26:59 -0600

    juntando ultimas modificaciones de gael

---
+++ src/main/java/datos/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datos;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class NewClass {
+     public static void main(String[] args) {
+
+        //Pruebas CONEXION
+        // PRUEBA LISTAR DEPARTAMENTOS
+        System.out.println("***************LISTANDO DEPARTAMENTO**********************");
+}
+}
